893b466c70df0314213791820e3e175e786614a7
erik-sandoval <erik.sandoval@icloud.com>
2019-06-06 01:24:14 -0700

    layout for endpoints

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const knexConfig = require('./knexfile');
 
 const db = knex(knexConfig.development);
 
-console.log(db);
-
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.get('/api/cohorts', (req, res) => {});
+server.get('/api/cohorts/:id', (req, res) => {});
+server.put('/api/cohorts/:id', (req, res) => {});
+server.delete('/api/cohorts/:id', (req, res) => {});
+server.get('/api/cohorts/:id/students', (req, res) => {});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+	console.log(`listening on localhost:${port}`);
+});
